9db06c68a505572f784fe51d6c549a14dc88f1eb
Diego27012001 <68134658+Diego27012001@users.noreply.github.com>
2020-07-10 22:03:15 -0500

    Update IniciarSesion.java

--- src/IniciarSesion.java
+++ src/IniciarSesion.java
@@ -17,7 +17,9 @@ public class IniciarSesion extends javax.swing.JFrame {
         initComponents();
         System.out.println("Hola Ctmr");
         System.out.println("Deja de Joder Arenoso de mrd ");
-        System.out.println("Hola violale dor de patos")
+        System.out.println("Hola violale dor de patos");
+         System.out.println("oscar te ayudamos a buscar a tu papá?");
+            
     }
 
     /**
